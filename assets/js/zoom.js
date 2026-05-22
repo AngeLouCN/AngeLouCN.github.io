5160d3ad862aa3a2693e410e666ad6abe211481f
AngeLouCN <79243162+AngeLouCN@users.noreply.github.com>
2026-05-22 22:59:58 +0000

    Deploying to gh-pages from @ AngeLouCN/AngeLouCN.github.io@ce70e313ef172c267caee883adfce69041e52c6e 🚀

---
+++ assets/js/zoom.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){medium_zoom=mediumZoom("[data-zoomable]",{margin:48,background:getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color")+"ee"})});
